c77ebcde132300f6bc8164ba5e7030e6b4750b87
alan-lam <alanlam00@gmail.com>
2018-08-23 19:15:31 -0700

    added some js files

---
+++ JavaScript/player.js
@@ -0,0 +1,6 @@
+class Player {
+  constructor (card1, card2) {
+    this.card1 = card1;
+    this.card2 = card2;
+  }
+}
---
+++ JavaScript/table.js
@@ -0,0 +1,9 @@
+class Table {
+  constructor (card1, card2, card3, card4, card5) {
+    this.card1 = card1;
+    this.card2 = card2;
+    this.card3 = card3;
+    this.card4 = card4;
+    this.card5 = card5;
+  }
+}
